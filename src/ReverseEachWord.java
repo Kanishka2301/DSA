2d7ea2b137d6c53792fa269ae1a296b9c35ee436
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-04-16 19:25:56 +0530

    reverse each word in  string

--- src/ReverseEachWord.java
+++ src/ReverseEachWord.java
@@ -1,20 +1,31 @@
+import java.util.*;
 public class ReverseEachWord {
-    public static void rev(String str){
-        String[] words=str.split(" ");
-        String revstr="";
-        for(int i=0;i<words.length;i++){
-            String word=words[i];
-            String nstr="";
-            for(int j=0;j<word.length();j++){
-               char ch=word.charAt(j);
-               nstr=ch+nstr;
-            }
-            revstr=revstr+nstr+" ";
+    public static String rev(String s){
+       char[] ch=s.toCharArray();
+       int len=ch.length,sp=0;
+       for(int ep=0;ep<len;ep++){
+           if(ep==len || ch[ep]==' '){
+               reverse(ch,sp,ep-1);
+               sp=ep+1;
+           }
+       }
+       return new String(ch);
+    }
+    public static void reverse(char[] ch,int sp,int ep){
+        while(sp<ep){
+            char temp=ch[sp];
+            ch[sp]=ch[ep];
+            ch[ep]=temp;
+            sp++;
+            ep--;
         }
-        System.out.println(str);
-        System.out.println(revstr);
     }
-    public static void main(String[] args){
-        rev("Java program");
-    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String s = sc.nextLine();
+        System.out.println(rev(s));
+}
 }
