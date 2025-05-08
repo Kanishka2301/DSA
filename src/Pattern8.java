ddbf96bd1be09bddbec2107943ee6ad8f1a1fd73
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-05-08 22:29:56 +0530

    pattern

---
+++ src/Pattern8.java
@@ -0,0 +1,21 @@
+//* * * *
+// * * *
+//  * *
+//   *
+public class Pattern8 {
+    public static void pat(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int s = 0; s < row; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < n - row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        pat(n);
+    }
+}
